9609e64b832b71522d8cbaf31922429e2cc73ae5
QingOu <qingqingou@gmail.com>
2022-02-04 23:42:40 -0500

    Add style and auto generate members

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const render = require("./src/page-template");
 const teamMembers = [];
 const arrayIds = [];
 
-function buildYourTeam() {
+function createManager() {
   console.log("Please build your team");
 
   inquirer
@@ -42,9 +42,10 @@ function buildYourTeam() {
         res.email,
         res.officeNumber
       );
-      teamMembers.push(manager);
-      arrayIds.push(res.id);
 
+      teamMembers.push(manager);
+
+      arrayIds.push(res.id);
       createMember();
     });
 }
@@ -56,7 +57,7 @@ function createMember() {
         type: "list",
         name: "memberChoice",
         message: "Which team members would you like to add?",
-        choices: ["engineer", "intern", "none"],
+        choices: ["engineer", "intern", "done adding"],
       },
     ])
     .then((choice) => {
@@ -68,7 +69,7 @@ function createMember() {
           createIntern();
           break;
         default:
-          createTheTeam();
+          buildTheTeam();
       }
     });
 }
@@ -100,8 +101,8 @@ function createEngineer() {
     .then((res) => {
       const engineer = new Engineer(res.name, res.id, res.email, res.github);
       teamMembers.push(engineer);
-      arrayIds.push(res.id);
 
+      arrayIds.push(res.id);
       createMember();
     });
 }
@@ -114,29 +115,37 @@ function createIntern() {
         name: "name",
         message: "What is the intern's name?",
       },
-      // {
-      //   type: "input",
-      //   name: "id",
-      //   message: "What is the engineer's id?",
-      // },
-      // {
-      //   type: "input",
-      //   name: "email",
-      //   message: "What is the engineer's email?",
-      // },
-      // {
-      //   type: "input",
-      //   name: "github",
-      //   message: "What is the engineer's github?",
-      // },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Where is your intern's school?",
+      },
     ])
-    .then(() => {
+    .then((res) => {
+      const intern = new Intern(res.name, res.id, res.email, res.school);
+      teamMembers.push(intern);
+
+      arrayIds.push(res.id);
       createMember();
     });
 }
 
-function createTheTeam() {
-  fs.writeFile("team.html", "", "utf-8");
+function buildTheTeam() {
+  fs.writeFile("dist/team.html", render(teamMembers), (err) => {
+    if (err) {
+      console.log("err", err);
+    }
+  });
 }
 
-buildYourTeam();
+createManager();
--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,98 @@
+const html = [];
+function generateMemberCards(data) {
+  function generateManagerCard(manager) {
+    return `
+      <div class="cardBorder">
+        <div class="cardHeader">
+          <label class="bigFont">${manager.name}</label>
+          <label class="medFont">Manager</label>
+        </div>
+        <div class="cardBody">
+          <p>ID: ${manager.id}</p>
+          <div class="divider"></div>
+          <p>Email: ${manager.email}</p>
+          <div class="divider"></div>
+          <p>Office Number:${manager.officeNumber}</p>
+        </div>
+      </div>
+  `;
+  }
+
+  function generateEngineerCard(engineer) {
+    return `
+    <div class="cardBorder">
+        <div class="cardHeader">
+          <label class="bigFont">${engineer.name}</label>
+          <label class="medFont">Engineer</label>
+        </div>
+        <div class="cardBody">
+          <p>ID: ${engineer.id}</p>
+          <div class="divider"></div>
+          <p>Email: ${engineer.email}</p>
+          <div class="divider"></div>
+          <p>Github: ${engineer.github}</p>
+        </div>
+      </div>
+
+  `;
+  }
+
+  function generateInternCard(intern) {
+    return `
+    <div class="cardBorder">
+        <div class="cardHeader">
+          <label class="bigFont">${intern.name}</label>
+          <label class="medFont">Intern</label>
+        </div>
+        <div class="cardBody">
+          <p>ID: ${intern.id}</p>
+          <div class="divider"></div>
+          <p>Email: ${intern.email}</p>
+          <div class="divider"></div>
+          <p>School: ${intern.school}</p>
+        </div>
+      </div>
+
+  `;
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].constructor.name === "Manager") {
+      html.push(generateManagerCard(data[i]));
+    } else if (data[i].constructor.name === "Engineer") {
+      html.push(generateEngineerCard(data[i]));
+    } else {
+      html.push(generateInternCard(data[i]));
+    }
+  }
+  return html.join("");
+}
+
 function generateTeamPage(data) {
-  console.log("data ", data);
+  return `
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="./styles.css" />
+        </head>
+      <body>
+        <div class="heroTitle">
+          <label>My Team</label>
+        </div>
+      
+        <div class="mainContainer">
+            ${generateMemberCards(data)}
+
+        </div>
+  
+    </body>
+    </html>
+
+  `;
 }
 
 module.exports = generateTeamPage;
